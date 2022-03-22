b2f63868c4f1297b1240b71bd74ddda06ebc421a
palwashamalik <palwasha.malik@devsinc.com>
2022-03-22 16:59:46 +0500

    Application: Edited Extreme Weather Implementation * Redone the code by using OOP concepts * Created Extremeweather Class * Created its method * Created a test object * Faced an issue of dynamic constant assignment

--- task1.rb
+++ task1.rb
@@ -1,45 +1,87 @@
-a=[ARGV]
-year=a[0][1].to_s
-path=a[0][2].to_s
-MainDirectoryfolder = Dir.glob("#{path}/*")
+
 require 'date'
-highest_temp = -1000
-lowest_temp = 1000
-most_humid = -1000
-most_humid_day = []
-lowest_temp_day = []
-highest_temp_day = []
-MainDirectoryfolder.each do |f|
-  subdirectoryfiles = Dir.glob("#{f}/*")
-  subdirectoryfiles.each do |filename|
-    day=[]
-    file = File.open(filename)
-    for line in file.readlines()[1..-1]
-      array=line.split(",")
-      day=array[0].split("-")
-      if day[0]==year
-        if array[1]!=""
-          if(array[1].to_i>=highest_temp.to_i)
-            highest_temp=array[1]
-            highest_temp_day=day
-          end
-        end
-        if array[3]!=""
-          if(array[3].to_i<=lowest_temp.to_i)
-            lowest_temp=array[3]
-            lowest_temp_day=day
-          end
-        end
-        if array[7]!=""
-          if(array[7].to_i>=most_humid.to_i)
-            most_humid=array[7]
-            most_humid_day=day
-          end
-        end
+class Extreme_weather
+
+  def initialize()
+    @most_humid_day = []
+    @lowest_temp_day = []
+    @highest_temp_day = []
+    @highest_temp = -1000
+    @lowest_temp = 1000
+    @most_humid = -1000
+
+  end
+
+  def print()
+    print("Highest: #{@highest_temp}C on #{Date::MONTHNAMES[@highest_temp_day[1].to_i]} #{@highest_temp_day[2]}\n")
+    print("Lowest: #{@lowest_temp}C on #{Date::MONTHNAMES[@lowest_temp_day[1].to_i]} #{@lowest_temp_day[2]}\n")
+    print("Humid: #{@most_humid}% on #{Date::MONTHNAMES[@most_humid_day[1].to_i]} #{@most_humid_day[2]}\n")
+
+
+  end
+
+  def find_highest_temp(temp,day)
+    if temp!=""
+      if(temp.to_i>=@highest_temp.to_i)
+        @highest_temp=temp
+        @highest_temp_day=day
       end
     end
   end
+
+  def find_lowest_temp(temp,day)
+    if temp!=""
+      if(temp.to_i<=@lowest_temp.to_i)
+        @lowest_temp=temp
+        @lowest_temp_day=day
+      end
+    end
+  end
+
+  def find_most_humid(humidity,day)
+    if humid!=""
+      if(humid.to_i>=@most_humid.to_i)
+        @most_humid=humidity
+        @most_humid_day=day
+      end
+    end
+  end
+
+
+  def readfile(file)
+    for line in file.readlines()[1..-1]
+      day=[]
+      array=line.split(",")
+      day=array[0].split("-")
+      if day[0]==$year
+        find_highest_temp(array[1],day)
+        find_highest_temp(array[3],day)
+        find_most_humid(array[7],day)
+      end
+    end
+
+
+  end
+
+  def iterate_folders(path)
+    MainDirectoryfolder = Dir.glob("#{path}/*")
+    MainDirectoryfolder.each do |f|
+      subdirectoryfiles = Dir.glob("#{f}/*")
+      subdirectoryfiles.each do |filename|
+        file = File.open(filename)
+        readfile(file)
+        file.close
+      end
+    end
+  end
+
+
+
 end
-print("Highest: #{highest_temp}C on #{Date::MONTHNAMES[highest_temp_day[1].to_i]} #{highest_temp_day[2]}\n")
-print("Lowest: #{lowest_temp}C on #{Date::MONTHNAMES[lowest_temp_day[1].to_i]} #{lowest_temp_day[2]}\n")
-print("Humid: #{most_humid}% on #{Date::MONTHNAMES[most_humid_day[1].to_i]} #{most_humid_day[2]}\n")
+
+a=[ARGV]
+year=a[0][1].to_s
+path=a[0][2].to_s
+testobj = Extreme_weather.new
+testobj.iterate_folders(path)
+testobj.print()
