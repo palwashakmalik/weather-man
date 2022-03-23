3dcb62baff106365a0905a55a9040f349f0eafd6
palwashamalik <palwasha.malik@devsinc.com>
2022-03-23 17:28:29 +0500

    Update Charts Weather Implementation * Added main file * Added read_file * Added weather_charts * Fixed errors and bugs * Fixed indentations and naming

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require_relative './Extreme_weather'
 require_relative './average_weather'
+require_relative './weather_charts'
 
 def user_input
-  raise 'Wrong User Input' unless ARGV[0].to_s == '-e' || ARGV[0].to_s == '-a'
+  raise 'Wrong User Input' unless ARGV[0].to_s == '-e' || ARGV[0].to_s == '-a' || ARGV[0].to_s == '-c'
 
   ExtremeWeather.new(ARGV[1].to_s) if ARGV[0].to_s == '-e'
   AverageWeather.new(ARGV[1].split('/')[0].to_s, ARGV[1].split('/')[1].to_s) if ARGV[0].to_s == '-a'
+  WeatherCharts.new(ARGV[1].split('/')[0].to_s, ARGV[1].split('/')[1].to_s) if ARGV[0].to_s == '-c'
 end
 begin
   object = user_input
---
+++ weather_charts.rb
@@ -0,0 +1,52 @@
+require 'colorize'
+require 'date'
+require_relative 'read_file'
+class WeatherCharts
+  include Readfile
+  def initialize(year, month)
+
+    puts "#{Date::MONTHNAMES[@month.to_i]} #{@year}\n "
+    @year = year
+    @month = month
+  end
+
+  def find_indexes(line)
+    @maximum_temperature_index = line.split(',').index('Max TemperatureC')
+    @minimum_temperature_index = line.split(',').index('Min TemperatureC')
+  end
+
+  def bars(count, color)
+    count.to_i.times do
+      if color == 'blue'
+        print('+'.blue)
+      else
+        print('+'.red)
+      end
+    end
+  end
+
+  def print_highest(temperature, day)
+    print("#{day} ")
+    bars(temperature, 'red')
+    print(" #{temperature}C\n")
+  end
+
+  def print_lowest(temperature, day)
+    print("#{day} ")
+    bars(temperature, 'blue')
+    print(" #{temperature}C\n")
+  end
+
+  def find_values(data)
+    data.shift if data[0].strip.empty?
+    find_indexes(data[0])
+    data[1..-2].each do |array|
+      array = array.split(',')
+      day = DateTime.strptime(array[0], '%Y-%m-%d')
+      return unless day.year.to_s == @year && day.month.to_s == @month
+
+      print_highest(array[@maximum_temperature_index], day.day) if !array[1].empty?
+      print_lowest(array[@maximum_temperature_index], day.day) if !array[3].empty?
+    end
+  end
+end
